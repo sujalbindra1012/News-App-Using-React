2fd2497cb23e6720228cc3631df111336a44ccac
sujalbindra1012 <sujalbindra@gmail.com>
2024-07-12 13:19:41 +0530

    spinner added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor() {
@@ -15,7 +16,7 @@ export class News extends Component {
 
   async componentDidMount() {
     console.log("CDM");
-    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -23,7 +24,7 @@ export class News extends Component {
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -35,8 +36,8 @@ export class News extends Component {
   }
 
   handleNextClick = async () => {
-    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / 20)) {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=${this.state.page + 1}&pageSize=20`;
+    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / this.props.pageSize)) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5779cb500dbf47e9a3dd3dfc195aab62&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -51,7 +52,8 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewsMonkey - Top Headlines</h1>
+        <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         {this.state.loading && <h2>Loading...</h2>}
         <div className='row'>
           {!this.state.loading && this.state.articles.map((element) => {
@@ -64,7 +66,7 @@ export class News extends Component {
         </div>
         <div className='container d-flex justify-content-between'>
           <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import loading from './Spinner-3.gif'
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img src={loading} alt='not generated'/>
+      </div>
+    )
+  }
+}
